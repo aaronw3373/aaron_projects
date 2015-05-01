5d600673e83f5f8f2aa731fbca99447d02cb7306
aaronw3373 <aaronw3373@gmail.com>
2015-05-01 07:37:00 -0400

    reorganizing folder'
    '

---
+++ js/battleship.js
@@ -0,0 +1,16 @@
+nodefunction setupBoard(x) {
+  return function(){
+    var board = [];
+    for (var i = 0; i < x; i++) {
+      board.push([]);
+      for (var f = 0; f < x; f++) {
+        board[i].push([]);
+      };
+    };
+    return board;
+  }
+}
+var userBoard = setupBoard(10);
+userBoard.board[0][0] = ['x'];
+console.log(userBoard());
+
